b108e20311269bf05c511cf7f17b71d9b031d94b
fanyui <fanyuiharisu@gmail.com>
2024-08-06 18:50:17 +0100

    fix: Template code for generating advise for a farm and a particular crop

--- modules/routers/farming_advice.py
+++ modules/routers/farming_advice.py
@@ -1,9 +1,12 @@
+from datetime import datetime
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from modules.database.models import FarmingAdvice
+from modules.database.models import Crop, FarmingAdvice, WeatherForecast
 from modules.database.schemas.farming_advice_schemas import FarmingAdviceCreate
 from modules.utilities.auth import get_db_session
+from modules.utilities.gemini_integration import get_advice_for_crop
 
 router = APIRouter(tags=["FarmingAdvice"])
 
@@ -18,3 +21,30 @@ def create_farming_advice(advice: FarmingAdviceCreate, db: Session = Depends(get
     db.commit()
     db.refresh(db_advice)
     return db_advice
+
+
+@router.get("/farming_advice/{advice_id}")
+def get_farming_advice(advice_id: int, db: Session = Depends(get_db_session)):  # noqa: B008
+    return db.query(FarmingAdvice).filter(FarmingAdvice.id == advice_id).first()
+
+
+@router.get("/farming_advice")
+def get_farming_advices(skip: int = 0, limit: int = 100, db: Session = Depends(get_db_session)):  # noqa: B008
+    return db.query(FarmingAdvice).offset(skip).limit(limit).all()
+
+
+# get farming advise for a crop
+@router.get("/farming_advice/crop/{crop_id}")
+def get_farming_advices_for_crop(crop_id: int, db: Session = Depends(get_db_session)):  # noqa: B008
+    crop = db.query(Crop).filter(Crop.id == crop_id).first()
+    if not crop:
+        return None
+    weather = (
+        db.query(WeatherForecast)
+        .filter(WeatherForecast.farm_id == crop.farm_id)
+        .filter(WeatherForecast.date > datetime.now())
+        .all()
+    )
+    crop_advise = get_advice_for_crop(crop, weather)
+    ## store the advise on the database
+    return crop_advise
