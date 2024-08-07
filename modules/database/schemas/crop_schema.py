b6ceafad016cbf6bace084753c0a4cbc63a84b16
Molinge <molingejr@gmail.com>
2024-08-07 09:54:53 +0100

    CRUD for crops

--- modules/database/schemas/crop_schema.py
+++ modules/database/schemas/crop_schema.py
@@ -23,6 +23,7 @@ class CropInDB(CropBase):
     id: int
     farm_id: int
     created_at: datetime
+    updated_at: datetime
 
     class Config:
         from_attributes = True
--- modules/routers/crop.py
+++ modules/routers/crop.py
@@ -1,17 +1,68 @@
-from fastapi import APIRouter, Depends
+from typing import List
+
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
 from modules.database.models import Crop
-from modules.database.schemas.crop_schema import CropCreate
+from modules.database.schemas.crop_schema import CropCreate, CropInDB, CropUpdate
 from modules.utilities.auth import get_db_session
 
 router = APIRouter(tags=["crop"])
 
 
-@router.post("/crop/", response_model=CropCreate)
+@router.post("/crop/", status_code=status.HTTP_201_CREATED, response_model=CropInDB)
 def create_crop(crop: CropCreate, db: Session = Depends(get_db_session)):  # noqa: B008
-    db_crop = Crop(crop_type=crop.crop_type, notes=crop.notes, planted_on=crop.planted_on, farm_id=crop.farm_id)
+    db_crop = Crop(
+        crop_type=crop.crop_type,
+        notes=crop.notes,
+        planted_on=crop.planted_on,
+        farm_id=crop.farm_id
+    )
     db.add(db_crop)
     db.commit()
     db.refresh(db_crop)
     return db_crop
+
+
+@router.get("/crops/", response_model=List[CropInDB])
+def get_crops(db: Session = Depends(get_db_session)):
+    return db.query(Crop).all()
+
+
+@router.get("/crops/{crop_id}", response_model=CropInDB)
+def get_crop(crop_id: int, db: Session = Depends(get_db_session)):
+    db_crop = db.query(Crop).filter_by(id=crop_id).first()
+    if not db_crop:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Crop not found")
+
+    return db_crop
+
+
+@router.put("/crops/{crop_id}", response_model=CropInDB)
+def update_crop(crop_id: int, crop: CropUpdate, db: Session = Depends(get_db_session)):
+    db_crop = db.query(Crop).filter(Crop.id == crop_id).first()
+
+    if not db_crop:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Crop not found")
+
+    db_crop.crop_type = crop.crop_type
+    db_crop.notes = crop.notes
+    db_crop.planted_on = crop.planted_on
+
+    db.commit()
+    db.refresh(db_crop)
+
+    return db_crop
+
+
+@router.delete("/crops/{crop_id}")
+def delete_crop(crop_id: int, db: Session = Depends(get_db_session)):
+    crop = db.query(Crop).filter(Crop.id == crop_id).first()
+
+    if not crop:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Crop not found")
+
+    db.delete(crop)
+    db.commit()
+
+    return {"message": "Crop deleted"}
