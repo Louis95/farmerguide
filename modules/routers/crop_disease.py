98546d8e5d16ad19614c32fea390304f95fc8834
pila louis <plouisasobo@gmail.com>
2024-08-12 10:47:43 +0200

    Add crop disease documentation

--- modules/routers/crop_disease.py
+++ modules/routers/crop_disease.py
@@ -3,29 +3,27 @@ from typing import List
 from fastapi import APIRouter, Depends, UploadFile
 from sqlalchemy.orm import Session
 
-from modules.database.models import CropDisease
 from modules.database.models.crop_diagnosis_models import CropDiagnosis
 from modules.database.schemas.crop_diagnosis_schemas import (
     CropDiagnosisBase,
     CropDiagnosisCreate,
 )
-from modules.database.schemas.crop_disease_schemas import CropDiseaseCreate
 from modules.utilities.auth import get_db_session
 from modules.utilities.gemini_integration import get_disease_data
 
 router = APIRouter(tags=["CropDiseases"])
 
 
-@router.post("/crop_diseases/", response_model=CropDiseaseCreate)
-def create_crop_disease(crop_disease: CropDiseaseCreate, db: Session = Depends(get_db_session)):  # noqa: B008
-    # AI integration for crop disease detection
-    # ai_response = requests.post("AI_API_URL", json=crop_disease.dict()).json()
-    # Parse AI response and update the crop_disease object if needed
-    db_crop_disease = CropDisease(**crop_disease.dict())
-    db.add(db_crop_disease)
-    db.commit()
-    db.refresh(db_crop_disease)
-    return db_crop_disease
+# @router.post("/crop_diseases/", response_model=CropDiseaseCreate)
+# def create_crop_disease(crop_disease: CropDiseaseCreate, db: Session = Depends(get_db_session)):  # noqa: B008
+#     # AI integration for crop disease detection
+#     # ai_response = requests.post("AI_API_URL", json=crop_disease.dict()).json()
+#     # Parse AI response and update the crop_disease object if needed
+#     db_crop_disease = CropDisease(**crop_disease.dict())
+#     db.add(db_crop_disease)
+#     db.commit()
+#     db.refresh(db_crop_disease)
+#     return db_crop_disease
 
 
 @router.post("/crop-diseases/detect", response_model=CropDiagnosisCreate)
@@ -52,6 +50,8 @@ async def detect_crop_disease(
 async def generic_detect_crop_disease(
     user_prompt: str, files: List[UploadFile], db: Session = Depends(get_db_session)  # noqa: B008
 ):
+    """Delete a Farm instance."""
+
     filePaths = []
     for file in files:
         file_location = f"uploaded_files/{file.filename}"
@@ -65,9 +65,11 @@ async def generic_detect_crop_disease(
 
 @router.get("/crop-diseases/{crop_diagnosis_id}", response_model=CropDiagnosisCreate)
 def get_crop_disease(crop_diagnosis_id: int, db: Session = Depends(get_db_session)):
+    """Get a crop disease instance."""
     return db.query(CropDiagnosis).filter(CropDiagnosis.id == crop_diagnosis_id).first()
 
 
 @router.get("/crop-diseases/history", response_model=List[CropDiagnosisCreate])
 def get_crop_disease_history(crop_id: int, db: Session = Depends(get_db_session)):
+    """Get a crop disease history."""
     return db.query(CropDiagnosis).filter(CropDiagnosis.crop_id == crop_id).all()
